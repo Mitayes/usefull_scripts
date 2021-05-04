739cc4420971f24ae8b908d27837ac0f644f3b34
Mitayes <30980693+Mitayes@users.noreply.github.com>
2021-05-04 15:46:02 +0500

    rework calculating variable outname
    Переписал порядок формирования outname (теперь через os.path.join для кроссплатформенности)

--- convert_xlsx2csv.py
+++ convert_xlsx2csv.py
@@ -5,12 +5,12 @@ import re
 
 
 # Функция, принимает аргументом 1 путь к папке, внутри которой размещены файлы xlsx, папок внутри быть не должно!
-def __convert_xlsx2csv__(input_fold):
-    for root, dirs, files in os.walk(input_fold):
+def __convert_xlsx2csv__(root_dir):
+    for root, dirs, files in os.walk(root_dir):
         for name in files:
             fullname = os.path.join(root, name)
             if os.path.isfile(fullname):
-                outname = str(root) + "\\converted\\" + str(re.sub(r'.xlsx', '.csv', name))
+                outname = os.path.join(root, 'converted', re.sub(r'.xlsx', '.csv', name))
                 # Тут указывается кодировка и разделитель, sheetid=0 - значит, что конвертим все листы из книги,
                 # иначе пишем номер нужного листа
                 Xlsx2csv(fullname, outputencoding="utf-8", delimiter='#').convert(outname, sheetid=0)
